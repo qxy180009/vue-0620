66a4eeb582ff887fa76d50155940baf75cffa7d2
eclipse_user <eclipse_user@atguigu.com>
2019-06-20 23:15:55 -0500

    项目第一天下班打卡

--- README.md
+++ README.md
@@ -12,3 +12,27 @@
 1. git add . 
 2. git commit -, "提交信息"
 3. git push
+
+## 制作首页中的App组件
+1. 完成 Header 区域, 使用的是Mint-UI 中的Header组件
+2. 制作底部的Tabbar区域, 使用的是MUI的Tabbar.html
+ + 先把扩展图标 css样式时, 拷贝到项目中
+ + 拷贝 扩展字体库 ttf文件 到项目中去
+ + 为购物车 小图标, 添加如下样式, `mui-icon mui-icon-extra mui-icon-extra-cart`
+3. 要在中间区域放置一个router-view来展示路由的组件
+
+## 改造 tabbar 为 router-link
+
+## 设置路由高亮
+
+## 点击 tabbar 中的路由连接,展示对应的路由组件
+
+## 制作首页轮播图布局
+
+## 加载首页轮播图数据
+1. 获取数据, 如何获取, 使用vue-resource
+2. 使用 vue-resource 的 this.$http.get获取数据
+3. 获取的数据保存到data身上
+4. 使用v-for渲染每个item项
+
+## 改造 九宫格 区域的样式
--- src/main.js
+++ src/main.js
@@ -1,18 +1,31 @@
 // 入口文件
 import Vue from 'vue'
+// 导入路由包
+import VueRouter from 'vue-router'
+// 1.2 安装路由
+Vue.use(VueRouter)
+
+// 2.1 导入vue-resource
+import VueResource from 'vue-resource'
+// 2.2 安装vue-resource
+Vue.use(VueResource)
 
 // 导入app根组件
 import app from './app.vue'
 
 // 导入MUI的样式
 import  './lib/mui/css/mui.min.css'
-
+import './lib/mui/css/icons-extra.css'
 import MintUI from 'mint-ui'
 
 import 'mint-ui/lib/style.css'
 Vue.use(MintUI)
 
+// 1.3 导入自己的router.js路由模块
+import router from './router.js'
+
 var vm = new Vue({
     el: '#app',
-    render: c => c(app)   
+    render: c => c(app),
+    router // 1.4 挂载路由对象到VM实例上
 })
